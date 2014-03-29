d828ed9f27b1f901fb9b6fd27154b9c3dbb2e1d0
Timothy Miller <t_miller@outlook.com>
2014-03-28 22:37:52 -0400

    Add view model to Recent Activity Page

---
+++ FoulPlay-Windows8/ViewModels/RecentActivityPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FoulPlay_Windows8.Common;
+using FoulPlay_Windows8.Tools;
+
+namespace FoulPlay_Windows8.ViewModels
+{
+    public class RecentActivityPageViewModel : NotifierBase
+    {
+        private RecentActivityScrollingCollection _recentActivityScrollingCollection;
+
+        public RecentActivityScrollingCollection RecentActivityScrollingCollection
+        {
+            get { return _recentActivityScrollingCollection; }
+            set
+            {
+                SetProperty(ref _recentActivityScrollingCollection, value);
+                OnPropertyChanged();
+            }
+        }
+
+        public void SetRecentActivityFeed()
+        {
+            RecentActivityScrollingCollection = new RecentActivityScrollingCollection
+            {
+                IsNews = false,
+                StorePromo = false,
+                UserAccountEntity = App.UserAccountEntity,
+                Username = App.UserAccountEntity.GetUserEntity().OnlineId,
+                PageCount = 0
+            };
+        }
+    }
+}
--- FoulPlay-Windows8/Views/RecentActivityPage.xaml.cs
+++ FoulPlay-Windows8/Views/RecentActivityPage.xaml.cs
@@ -18,6 +18,7 @@ using Windows.UI.Xaml.Navigation;
 using Foulplay_Windows8.Core.Entities;
 using Foulplay_Windows8.Core.Managers;
 using FoulPlay_Windows8.Tools;
+using FoulPlay_Windows8.ViewModels;
 using Newtonsoft.Json;
 
 namespace FoulPlay_Windows8.Views
@@ -29,16 +30,8 @@ namespace FoulPlay_Windows8.Views
     {
 
         private NavigationHelper navigationHelper;
-        private ObservableDictionary defaultViewModel = new ObservableDictionary();
         private UserAccountEntity.User _user;
-
-        /// <summary>
-        /// This can be changed to a strongly typed view model.
-        /// </summary>
-        public ObservableDictionary DefaultViewModel
-        {
-            get { return this.defaultViewModel; }
-        }
+        private RecentActivityPageViewModel _vm;
 
         /// <summary>
         /// NavigationHelper is used on each page to aid in navigation and 
@@ -80,7 +73,7 @@ namespace FoulPlay_Windows8.Views
                 App.UserAccountEntity.SetUserEntity(_user);
             }
             _user = App.UserAccountEntity.GetUserEntity();
-            LoadRecentActivityList();
+            _vm.SetRecentActivityFeed();
         }
 
         /// <summary>
@@ -99,41 +92,6 @@ namespace FoulPlay_Windows8.Views
             e.PageState["userEntity"] = jsonObjectString;
         }
 
-        private async void LoadRecentActivityList()
-        {
-            //LoadingProgressBar.Visibility = Visibility.Visible;
-            var recentActivityCollection = new RecentActivityScrollingCollection
-            {
-                IsNews = false,
-                StorePromo = true,
-                UserAccountEntity = App.UserAccountEntity,
-                Username = _user.OnlineId,
-                PageCount = 1
-            };
-            var recentActivityManager = new RecentActivityManager();
-            var recentActivityEntity =
-                await recentActivityManager.GetActivityFeed(_user.OnlineId, 0, true, false, App.UserAccountEntity);
-            if (recentActivityEntity == null)
-            {
-                //recentActivityCollection = null;
-                //ActivityFeedListView.DataContext = recentActivityCollection;
-                //NoActivitiesTextBlock.Visibility = Visibility.Visible;
-                //LoadingProgressBar.Visibility = Visibility.Collapsed;
-                return;
-            }
-            if (recentActivityEntity.feed != null)
-            {
-                //NoActivitiesTextBlock.Visibility = Visibility.Collapsed;
-                foreach (var item in recentActivityEntity.feed)
-                {
-                    recentActivityCollection.Add(item);
-                }
-            }
-            DefaultViewModel["RecentActivityList"] = recentActivityCollection;
-            //RecentActivityHubSection.DataContext = recentActivityCollection;
-            //LoadingProgressBar.Visibility = Visibility.Collapsed;
-        }
-
         #region NavigationHelper registration
 
         /// The methods provided in this section are simply used to allow
@@ -147,6 +105,7 @@ namespace FoulPlay_Windows8.Views
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+            _vm = (RecentActivityPageViewModel) DataContext;
             navigationHelper.OnNavigatedTo(e);
         }
 
