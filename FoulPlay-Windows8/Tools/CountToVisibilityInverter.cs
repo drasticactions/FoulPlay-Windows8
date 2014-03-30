13575a1d2d52532dd708b458556a999d389f14c4
Timothy Miller <t_miller@outlook.com>
2014-03-30 17:09:44 -0400

    Added "No" object messages.

--- FoulPlay-Windows8/Tools/CountToVisibilityInverter.cs
+++ FoulPlay-Windows8/Tools/CountToVisibilityInverter.cs
@@ -1,16 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Data;
 
 namespace FoulPlay_Windows8.Tools
 {
-    public class CountToVisibilityConverter : IValueConverter
+    public class CountToVisibilityInverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
             if (value == null)
-                return Visibility.Collapsed;
-            return (int)value <= 0 ? Visibility.Collapsed : Visibility.Visible;
+                return Visibility.Visible;
+            return (int)value <= 0 ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
